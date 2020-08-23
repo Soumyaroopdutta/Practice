3413b4a97c2caac875b3c1fdb207cecf36c041ec
soumyaroopdutta <soumyaroopdutta@gmail.com>
2020-08-22 23:57:06 -0700

    Try 1, but has bugs
    there seems to be a bug when the consumer doesn't know the done
    flag has been released while going to cv.wait.

--- synchro/prodConsumer.cpp
+++ synchro/prodConsumer.cpp
@@ -2,28 +2,38 @@
 #include <thread>
 #include <condition_variable>
 #include <mutex>
+#include <atomic>
 #include <chrono>
 #include <queue>
 using namespace std;
 
 // BUGGY PROGRAM. SOLVE IT
+condition_variable cv;
+mutex mtx;
 
 int main() {
     int c = 0;
-    bool done = false;
+    atomic<bool> done(false);
     queue<int> goods;
 
     thread producer([&]() {
         for (int i = 0; i < 500; ++i) {
+            mtx.lock();
             goods.push(i);
             c++;
+            cv.notify_one();
+            mtx.unlock();
         }
 
         done = true;
+        cv.notify_one();
+        mtx.unlock();
     });
 
     thread consumer([&]() {
         while (!done) {
+            unique_lock<mutex> lck(mtx);
+            cv.wait(lck);
             while (!goods.empty()) {
                 goods.pop();
                 c--;
